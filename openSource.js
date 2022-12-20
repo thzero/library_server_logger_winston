fcd1b93858cc4eab6f64bd6e13bb135bcd884ae8
thzero <thzero@users.noreply.github.com>
2022-12-20 07:51:13 -0600

    updates for opensource

---
+++ openSource.js
@@ -0,0 +1,53 @@
+export default () => {
+    return [
+        {
+            category: 'server',
+            name: '@thzero/library_common',
+            url: 'https://github.com/thzero/library_common',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_common/blob/master/license.md'
+        },
+        {
+            category: 'server',
+            name: '@thzero/library_common_service',
+            url: 'https://github.com/thzero/library_common_service',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_common_service/blob/master/license.md'
+        },
+        {
+            category: 'server',
+            name: '@thzero/library_service',
+            url: 'https://github.com/thzero/library_service',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_service/blob/master/license.md'
+        },
+		{
+			category: 'server',
+			name: '@thzero/library_server_logger_winston',
+			url: 'https://github.com/thzero/library_server_logger_winston',
+			licenseName: 'MIT',
+			licenseUrl: 'https://github.com/thzero/library_server_logger_winston/blob/master/license.md'
+		},
+		{
+			category: 'server',
+			name: '@thzero/winston-syslog',
+			url: 'https://github.com/thzero/winston-syslog',
+			licenseName: 'MIT',
+			licenseUrl: 'https://github.com/thzero/winston-syslog/blob/master/license.md'
+		},
+		{
+			category: 'server',
+			name: 'winston',
+			url: 'https://github.com/winstonjs/winston',
+			licenseName: 'MIT',
+			licenseUrl: 'https://github.com/winstonjs/winston/blob/master/LICENSE'
+		},
+		{
+			category: 'server',
+			name: 'winston',
+			url: 'https://github.com/winstonjs/winston',
+			licenseName: 'MIT',
+			licenseUrl: 'https://github.com/winstonjs/winston/blob/master/LICENSE'
+		}
+    ];
+}
